f39c7009af0d29b38cbd68cb100d458d6d6eed01
benoitlahoz <lahoz.ben@gmail.com>
2025-01-09 14:46:00 +0100

    Added comments.

--- src/assemblage/types.ts
+++ src/assemblage/types.ts
@@ -6,11 +6,30 @@ import type { AssemblerContext } from '@/assembler/types';
 export abstract class AbstractAssemblage {
   [key: string]: any;
 
+  /**
+   * Called on concrete class registration by assembler.
+   *
+   * @param { AssemblerContext } context The assembler's context.
+   */
   // @ts-expect-error 'context' is unused.
   static onRegister(context: AssemblerContext): void {}
 
+  /**
+   * Called when class is instantiated.
+   *
+   * @param { AssemblerContext } context The assembler's context.
+   */
   public abstract onInit?(context: AssemblerContext): void | Promise<void>;
+
+  /**
+   * Called when instance of class is disposed.
+   *
+   * @param { AssemblerContext } context The assembler's context.
+   */
   public abstract onDispose?(context: AssemblerContext): void;
 
+  /**
+   * Dispose the assemblage instance.
+   */
   public abstract dispose?(): void;
 }
