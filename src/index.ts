b5556d1cc892d1414410d5d61d13b6a0ed7ea419
benoitlahoz <lahoz.ben@gmail.com>
2025-01-09 14:47:44 +0100

    Reflect changes in parameters decorators.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 export { AbstractAssemblage } from '@/assemblage/types';
 export { Assemblage } from '@/assemblage/decorator';
-
+export { Context, Configuration, Definition } from '@/injection/decorators';
 export type { AssemblerContext } from '@/assembler/types';
 export { AbstractAssembler } from '@/assembler/types';
 export { Assembler } from '@/assembler/assembler';
-
-export { Context, Configuration, Metadata } from '@/assembler/decorators';
