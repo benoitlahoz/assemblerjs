67a26787e0c2dab11f4e612576bff5b75ecbd78f
benoitlahoz <lahoz.ben@gmail.com>
2025-01-09 17:08:30 +0100

    Update 'Assembler' abstract class.

--- src/assembler/types.ts
+++ src/assembler/types.ts
@@ -1,5 +1,6 @@
 import type { Identifier } from '@/types';
 import type { Injection } from '@/injection/types';
+import type { Injectable } from '@/injection/injectable';
 
 export interface AssemblerContext {
   register: AbstractAssembler['register'];
@@ -12,7 +13,7 @@ export abstract class AbstractAssembler {
   public abstract context: AssemblerContext;
   public abstract size: number;
 
-  public abstract register<T>(injection: Injection<T>): void;
+  public abstract register<T>(injection: Injection<T>): Injectable<T>;
   public abstract has<T>(identifier: Identifier<T>): boolean;
   public abstract require<T>(identifier: Identifier<T>): T;
   public abstract tagged(...tags: string[]): any[];
