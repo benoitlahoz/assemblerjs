e2a9b01dd9151a9288b36403bd88f185076dc43b
benoitlahoz <lahoz.ben@gmail.com>
2025-01-15 09:39:11 +0100

    Corrected export.

--- src/assemblage/index.ts
+++ src/assemblage/index.ts
@@ -1,6 +1,4 @@
 export * from './abstract';
 export * from './decorator';
-export * from '../injectable/dependencies';
-export * from '../common/reflection';
 export * from './definition';
 export * from './helpers';
