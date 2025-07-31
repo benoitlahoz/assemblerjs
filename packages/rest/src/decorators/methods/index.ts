8f264c2b070b8651814f1e414396fede95bc3aa8
Benoît LAHOZ <lahoz.ben@gmail.com>
2025-07-31 16:25:01 +0200

    Missing export

--- packages/rest/src/decorators/methods/index.ts
+++ packages/rest/src/decorators/methods/index.ts
@@ -1,5 +1,6 @@
 export * from './http-methods.decorators';
 export * from './http-methods.types';
 export * from './http-status.decorator';
+export * from './http-headers.decorator';
 export * from './redirect.decorator';
 export * from './middleware.decorators';
