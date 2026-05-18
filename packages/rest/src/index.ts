ab75d0c4b162ecd2b7e4f700314d9b2d0fe025c4
Benoît LAHOZ <lahoz.ben@gmail.com>
2026-05-18 09:53:01 +0200

    Update index.ts

--- packages/rest/src/index.ts
+++ packages/rest/src/index.ts
@@ -1,5 +1,4 @@
 export * from './adapters';
-export * from './decorators/constructor/deprecated/basic-controller.decorator';
 export * from './decorators/constructor/controller.decorator';
 export * from './decorators/methods';
 export * from './decorators/parameters';
