e1df2f6d8a71d54664cf029bfd3fbe000e7f7ebb
Benoît LAHOZ <lahoz.ben@gmail.com>
2026-06-02 14:51:25 +0200

    Update i18n.service.ts

--- examples/electron-01/src/features/i18n/main/i18n.service.ts
+++ examples/electron-01/src/features/i18n/main/i18n.service.ts
@@ -3,15 +3,15 @@ import { AbstractAssemblage, Assemblage } from 'assemblerjs';
 @Assemblage()
 export class I18nService implements AbstractAssemblage {
   private readonly dictionary: Record<string, string> = {
-    'menu.group.app': 'Application',
-    'menu.group.window': 'Fenetre',
-    'menu.group.developer': 'Developpeur',
-    'menu.group.refresh': 'Rafraichir',
-    'menu.window.refreshBounds': 'Rafraichir les dimensions',
-    'menu.window.randomBounds': 'Dimensions aleatoires',
-    'menu.window.centerWindow': 'Centrer la fenetre',
-    'menu.window.autoCenter': 'Auto-centrer apres aleatoire',
-    'menu.app.quit': 'Quitter',
+    'menu.group.app': 'App',
+    'menu.group.window': 'Window',
+    'menu.group.developer': 'Developer',
+    'menu.group.refresh': 'Refresh',
+    'menu.window.refreshBounds': 'Refresh Bounds',
+    'menu.window.randomBounds': 'Random Bounds',
+    'menu.window.centerWindow': 'Center Window',
+    'menu.window.autoCenter': 'Auto-center after random',
+    'menu.app.quit': 'Quit',
   };
 
   public translate(key: string): string {
