3452385da39d898a10997451f2a1681a7d772d9f
Benoît LAHOZ <lahoz.ben@gmail.com>
2026-06-07 11:24:11 +0200

    Update menu-orchestrator.decorator.ts

--- packages/electron/src/menu/main/menu-orchestration/menu-orchestrator.decorator.ts
+++ packages/electron/src/menu/main/menu-orchestration/menu-orchestrator.decorator.ts
@@ -1,4 +1,5 @@
 import { composeDecorators } from '@assemblerjs/core';
+import { AppListener } from '@/app/main/app-listener.decorator';
 // Direct import to avoid barrel circular dependencies
 import { MenuController } from '../menu-controller/menu-controller.decorator';
 
@@ -6,4 +7,4 @@ import { MenuController } from '../menu-controller/menu-controller.decorator';
  * Compose main-process menu lifecycle decorator.
  */
 export const MenuOrchestrator = (): ClassDecorator =>
-  composeDecorators(MenuController());
+  composeDecorators(MenuController(), AppListener());
